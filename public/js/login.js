8a825d30f7b444bf52db0611c2c9b473c627a057
Pixls112 <nguyen.phillip112@gmai.com>
2023-11-06 18:09:12 -0800

    added js code to public js files

---
+++ public/js/login.js
@@ -0,0 +1,24 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const name = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (name && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ name, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert("Login failed wrong username or passowrd");
+      }
+    }
+  };
+  
+  document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
---
+++ public/js/signUp.js
@@ -0,0 +1,24 @@
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const name = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (name && password) {
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ name, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        window.location.replace('/profile');
+      } else {
+        alert("Please enter a valid name or password");
+      }
+    }
+  };
+  
+  document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
