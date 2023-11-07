35ae414851e1b3669ffa32288f047ed0c057344a
Pixls112 <nguyen.phillip112@gmai.com>
2023-11-06 19:59:19 -0800

    added handlebars and npm i dependencies

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -79,4 +79,29 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+router.get('/signUp', async (req, res) => {
+  try {
+    // Pass serialized data and session flag into template
+    res.render('signUp');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/signUp', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_name = userData.name;
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 module.exports = router;
